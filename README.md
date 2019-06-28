bb668db0d6ff7208df5b1185e86fbf9cddacc845
Андрей <anderkoval3@gmail.com>
2019-06-28 14:10:41 +0300

    setting tommorow and the day after tomorrow date. Fixed buttons and other minor fixes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RemindMe_TelegramBot
 A simple reminder telegram bot
+To use calendar 
+```npm i telegraf-calendar-telegram --save```
--- bot.js
+++ bot.js
@@ -6,13 +6,21 @@ const TOKEN = require('./bot_token.json')['token']
 var TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(TOKEN, { polling: true, timeout: 500 });
 
-rawdata = fs.readFileSync('users.json');
-var USERS = JSON.parse(rawdata)
+rawdata = fs.readFileSync('users.json'); //@todo fix this
+var USERS = JSON.parse(rawdata ,function(key, value) {
+    if (key == "date") {
+        return new Date(value)
+    } else {
+        return value
+    }
+})
+
 
 var NOTES = require('./notes.json')
 const default_note = {
     text: '',
     date: 0,
+    time: 0,
     user_id: 0
 }
 
@@ -20,7 +28,7 @@ function getUserBufferNote(id) {
     for (let i in USERS) {
         if (USERS[i].id == id) {
             return USERS[i].buffer_note
-        }
+        } // @todo replace all for(...) with forEach(...)
     }
     return null
 }
@@ -44,7 +52,6 @@ function getUserById(id) {
     }
 }
 function checkUserData(user) {
-    user.buffer_note = default_note
     user.buffer_note.user_id = user.id
     if (!usersContain(user)) {
         user.cat_count = 0
@@ -68,8 +75,10 @@ var keyboard_anwers = {
     "Другая_дата": 9,
     "Назад_создание_заметки": 10,
     "Назад_изменение_даты": 11,
-    "Назад_изменение_время": 12
+    "Назад_изменение_время": 12,
+    "Созать_заметку": 13
 }
+
 console.log(keyboard_anwers.Создать_заметку)
 
 function onStartMsg(id) {
@@ -81,13 +90,52 @@ function onStartMsg(id) {
 }
 
 function onStart(user) {
+    user.buffer_note = default_note
     checkUserData(user)
     onStartMsg(user.id)
+}
+
+function note_menu_new_msg(id) {
+    let opts = { // @todo put this into func
+        reply_markup: create_note_markup
+    }
+    let text = JSON.stringify(getUserBufferNote(id))
+    if (text == undefined) {
+        text = "default debug note"
+    }
+    bot.sendMessage(id, text, opts)
+}
+
+function note_menu_edit_msg(chat_id, message_id) {
+    let opts = { // @todo put this into func
+        chat_id: chat_id,
+        message_id: message_id,
+        reply_markup: create_note_markup
+    }
+    text = JSON.stringify(getUserBufferNote(chat_id))
+    if (text == undefined) {
+        text = "default debug note"
+    }
+    bot.editMessageText(text, opts)
 }
 
 bot.onText(/start/, function (msg, match) {
     onStart(msg.from);
 });
+
+bot.on('message', function(msg){
+    let user = getUserById(msg.chat.id)
+    if (user.state == keyboard_anwers.Изменить_текст) {
+        user.buffer_note.text = msg.text
+        user.state = 0
+        var opts = {
+            reply_markup: create_note_markup
+        }
+        checkUserData(user)
+        note_menu_new_msg(user.id)
+    }
+});
+
 
 var HttpClient = function () {
     this.get = function (aUrl, aCallback) {
@@ -113,13 +161,13 @@ function sendCat(id) {
     var client = new HttpClient();
     client.get('https://api.thecatapi.com/v1/images/search', function (response) {
         let image_url = JSON.parse(response)[0]['url']
-        bot.sendPhoto(id, image_url) //@todo reply markup and always editing messages
+        bot.sendPhoto(id, image_url) 
     });
 }
 
 
 bot.onText(/cat/, function (msg, match) {
-    sendCat(user.id)
+    sendCat(msg.chat.id)
 });
 
 var on_start_markup = JSON.stringify({
@@ -132,10 +180,9 @@ var on_start_markup = JSON.stringify({
 
 var create_note_markup = JSON.stringify({
     inline_keyboard: [
-        [{ text: 'Изменить дату', callback_data: keyboard_anwers.Изменить_дату }],
-        [{ text: 'Изменить время', callback_data: keyboard_anwers.Изменить_время }],
+        [{ text: 'Изменить дату', callback_data: keyboard_anwers.Изменить_дату }, { text: 'Изменить время', callback_data: keyboard_anwers.Изменить_время }],
         [{ text: 'Изменить текст', callback_data: keyboard_anwers.Изменить_текст }],
-        [{ text: 'Назад', callback_data: keyboard_anwers.Назад_создание_заметки }]
+        [{ text: 'Назад', callback_data: keyboard_anwers.Назад_создание_заметки }, { text: 'Создать заметку', callback_data: keyboard_anwers.Создать_заметку }]
     ]
 })
 
@@ -159,14 +206,7 @@ bot.on('callback_query', function onCallbackQuery(callbackQuery) {
     // };
 
     if (action == keyboard_anwers.Создать_заметку) {
-        let opts = { // @todo put this into func
-            reply_markup: create_note_markup
-        }
-        text = JSON.stringify(getUserBufferNote(msg.chat.id))
-        if (text == undefined) {
-            text = "default debug note"
-        }
-        bot.sendMessage(msg.chat.id, text, opts)
+        note_menu_new_msg(msg.chat.id)
     } else if (action == keyboard_anwers.Получить_котика) {
         sendCat(msg.chat.id)
     } else if (action == keyboard_anwers.Изменить_дату) {
@@ -183,20 +223,36 @@ bot.on('callback_query', function onCallbackQuery(callbackQuery) {
     } else if (action == keyboard_anwers.Назад_создание_заметки){
         onStartMsg(msg.chat.id)
     } else if (action == keyboard_anwers.Назад_изменение_даты ){ 
-        let opts = { // @todo put this into func
-            chat_id: msg.chat.id,
-            message_id: msg.message_id,
-            reply_markup: create_note_markup
-        }
-        text = JSON.stringify(getUserBufferNote(msg.chat.id))
-        if (text == undefined) {
-            text = "default debug note"
-        }
-        bot.editMessageText(text, opts)
+        note_menu_edit_msg(msg.chat.id, msg.message_id)
+    } else if (action == keyboard_anwers.Изменить_текст) {
+        let user = getUserById(msg.chat.id)
+        user['state'] = keyboard_anwers.Изменить_текст
+        text = "Введите текст для заметки"
+        bot.sendMessage(msg.chat.id, text)
+        checkUserData(user)
+    } else if (action == keyboard_anwers.Сегодня) {
+        let user = getUserById(msg.chat.id)
+        user.buffer_note.date = new Date()
+        checkUserData(user)
+        note_menu_edit_msg(msg.chat.id, msg.message_id)
+    } else if (action == keyboard_anwers.Завтра) {
+        let user = getUserById(msg.chat.id)
+        let date = new Date()
+        date.setDate(date.getDate() + 1)
+        user.buffer_note.date = date
+        checkUserData(user)
+        note_menu_edit_msg(msg.chat.id, msg.message_id)
+    } else if (action == keyboard_anwers.Послезавтра) {
+        let user = getUserById(msg.chat.id)
+        let date = new Date()
+        date.setDate(date.getDate() + 2)
+        user.buffer_note.date = date
+        checkUserData(user)
+        note_menu_edit_msg(msg.chat.id, msg.message_id)
     }
-
-
 });
+    
+    
 
 
 
